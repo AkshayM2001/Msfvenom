e588f230dacb771b5c3ad0710627d56cd4a44dc3
AkshayM2001 <akshaymatale2001@gmail.com>
2024-06-16 21:50:59 +0530

    render ready

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from .models import SMSData
+from django.shortcuts import render
 
 @csrf_exempt
 def receive_data(request):
@@ -20,3 +21,11 @@ def receive_data(request):
             return JsonResponse({'status': 'error', 'message': str(e)})
     else:
         return HttpResponse(status=405)  # Method Not Allowed
+    
+
+def display_data(request):
+    data = SMSData.objects.all()
+    return render(request, 'display_data.html', {'data': data})
+
+def test_connection(request):
+    return JsonResponse({'status': 'success', 'message': 'Connection successful!'})
